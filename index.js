ddce1baa3c7eb40ec3c0ada160e660bec8c981a5
Bao <bao-tran.cong-huyen-ton-nu.1@ens.etsmtl.ca>
2020-07-09 10:53:56 -0400

    Add middleware

--- index.js
+++ index.js
@@ -3,6 +3,20 @@ const app = express()
 
 app.use(express.json())
 
+const requestLogger = (request, response, next)=> {
+  console.log('Method:', request.method)
+  console.log('Path:  ', request.path)
+  console.log('Body:  ', request.body)
+  console.log('---')
+  next()
+}
+
+const unknownEndpoint = (request, response) => {
+  response.status(404).send({ error: 'unknown endpoint' })
+}
+
+app.use(requestLogger) 
+
 let notes = [
   {
     id: 1,
@@ -43,13 +57,6 @@ app.get('/api/notes/:id', (req, res) => {
   }
 })
 
-app.delete('/api/notes/:id', (req, res) => {
-  const id = Number(req.params.id)
-  notes = notes.filter(note => note.id !== id)
-
-  res.status(204).end()
-})
-
 const generateId = () => {
   const maxId = notes.length > 0 ? Math.max(...notes.map(note => note.id)) : 0
   return maxId + 1
@@ -76,6 +83,15 @@ app.post('/api/notes',(req, res) => {
   notes = notes.concat(note)
   res.json(note)
 })
+
+app.delete('/api/notes/:id', (req, res) => {
+  const id = Number(req.params.id)
+  notes = notes.filter(note => note.id !== id)
+
+  res.status(204).end()
+})
+
+app.use(unknownEndpoint)
 
 const PORT = 3001
 
